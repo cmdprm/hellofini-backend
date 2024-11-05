8746c14680eb3d6fa3ebada3e80556b9e4cf4f5c
cmdprm <2badhermit@gmail.com>
2024-11-06 02:20:50 +0300

    Store User Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,16 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'telegram_id' => 'required|string|unique:users',
+            'telegram_id' => 'required|string',
             'first_name' => 'required|string|max:255',
             'username' => 'nullable|string|max:255',
         ]);
+
+        $user = User::where('telegram_id', $validatedData['telegram_id'])->first();
+
+        if ($user) {
+            return response()->json($user, 200);
+        }
 
         $user = User::create([
             'telegram_id' => $validatedData['telegram_id'],
